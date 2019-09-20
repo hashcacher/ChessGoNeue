04d37517b4fa002fbe03e4fe217e496927cfcbd6
Greg Guterman <gregoryguterman@gmail.com>
2019-09-20 16:14:19 -0700

    Add matchrequest tests

---
+++ Server/matchRequest/test_util.go
@@ -0,0 +1,25 @@
+package matchRequest
+
+import (
+	"net/http/httptest"
+)
+
+type TestResponseRecorder struct {
+	*httptest.ResponseRecorder
+	closeChannel chan bool
+}
+
+func (r *TestResponseRecorder) CloseNotify() <-chan bool {
+	return r.closeChannel
+}
+
+func (r *TestResponseRecorder) closeClient() {
+	r.closeChannel <- true
+}
+
+func NewTestResponseRecorder() *TestResponseRecorder {
+	return &TestResponseRecorder{
+		httptest.NewRecorder(),
+		make(chan bool, 1),
+	}
+}
