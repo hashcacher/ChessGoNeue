c73c15d2e37de8991fc1db9693b37733bedcefb0
Greg Guterman <gregoryguterman@gmail.com>
2019-09-20 20:43:20 -0700

    Oops forgot an import

--- Server/website.go
+++ Server/website.go
@@ -1,9 +1,11 @@
 package main
 
+import "net/http"
+
 func (s *Server) webHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write("Hello World!")
+	w.Write([]byte("Hello World!"))
 }
 
 func (s *Server) dingHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write("Dong")
+	w.Write([]byte("Dong"))
 }
