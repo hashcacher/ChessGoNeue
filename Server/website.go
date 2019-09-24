28f9fa6c58763f1e26ced783db0153ed9a559e1b
Greg Guterman <gregoryguterman@gmail.com>
2019-09-23 22:24:42 -0700

    Add content-type html

--- Server/website.go
+++ Server/website.go
@@ -9,6 +9,7 @@ import (
 var nextCheck time.Time
 
 func (s *Server) webHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte(s.poorMansHTML()))
 }
 
